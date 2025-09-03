1a49f4192a49be1bf34fad2530112f8c2c96e8a0
fguedez1311 <46085315+fguedez1311@users.noreply.github.com>
2025-09-03 15:55:23 -0400

    Terminado el semaforo  con use state y effect

---
+++ src/HookApp.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const HookApp = () => {
+  return (
+    <div className='bg-gradient'>
+        <h1 className='text-3xl font-bold underline'>Hola Mundo</h1>
+    </div>
+  )
+}
---
+++ src/main.tsx
@@ -0,0 +1,17 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+// import { HookApp } from './HookApp'
+import './index.css'
+// import { TrafficLight } from './01-useState/TrafficLight'
+// import { TrafficLightWithEffect } from './02-useEfect/TrafficLightWithEffect'
+import { TrafficLightWithHook } from './02-useEfect/TrafficLightWithHook'
+
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    {/* <HookApp /> */}
+    {/* <TrafficLight /> */}
+    {/* <TrafficLightWithEffect /> */}
+    <TrafficLightWithHook />
+  </StrictMode>,
+)
