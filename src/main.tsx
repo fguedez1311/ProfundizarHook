961b047fd0623662245c18f9dfabd905683ec3f0
fguedez1311 <46085315+fguedez1311@users.noreply.github.com>
2025-09-10 05:45:42 -0400

    Diseño con useReducer el scrambleWords

--- src/main.tsx
+++ src/main.tsx
@@ -8,10 +8,6 @@ import { ScrambleWords } from './o5-useReducer/ScrambleWords'
 // import { TrafficLightWithHook } from './02-useEfect/TrafficLightWithHook'
 // import { PokemonPage } from './03-examples/PokemonPage'
 // import { TasksApp } from './o5-useReducer/TaskApp'
-<<<<<<< HEAD
-=======
-import { ScrambleWords } from './o5-useReducer/ScrambleWords'
->>>>>>> a8e2cafeeaddaa57efd1eca80a8f77922bd88262
 
 
 createRoot(document.getElementById('root')!).render(
@@ -23,9 +19,6 @@ createRoot(document.getElementById('root')!).render(
     {/* <PokemonPage/> */}
     {/* <TasksApp /> */}
     <ScrambleWords />
-<<<<<<< HEAD
-=======
 
->>>>>>> a8e2cafeeaddaa57efd1eca80a8f77922bd88262
   </StrictMode>,
 )
