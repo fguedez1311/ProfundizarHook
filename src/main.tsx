6d06740573f7d3b16775ef550869a796b5d1a81a
fguedez1311 <46085315+fguedez1311@users.noreply.github.com>
2025-09-10 05:23:03 -0400

    desarrollando screambleWords con useState

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 // import { HookApp } from './HookApp'
 import './index.css'
+import { ScrambleWords } from './o5-useReducer/ScrambleWords'
 // import { TrafficLight } from './01-useState/TrafficLight'
 // import { TrafficLightWithEffect } from './02-useEfect/TrafficLightWithEffect'
 // import { TrafficLightWithHook } from './02-useEfect/TrafficLightWithHook'
 // import { PokemonPage } from './03-examples/PokemonPage'
-import { TasksApp } from './o5-useReducer/TaskApp'
+// import { TasksApp } from './o5-useReducer/TaskApp'
 
 
 createRoot(document.getElementById('root')!).render(
@@ -16,6 +17,7 @@ createRoot(document.getElementById('root')!).render(
     {/* <TrafficLightWithEffect /> */}
     {/* <TrafficLightWithHook /> */}
     {/* <PokemonPage/> */}
-    <TasksApp />
+    {/* <TasksApp /> */}
+    <ScrambleWords />
   </StrictMode>,
 )
