90fe30c95666ebc113a9f49fd103f56d07f43fc7
fguedez1311 <46085315+fguedez1311@users.noreply.github.com.>
2025-09-12 15:13:49 -0400

    Preparación del ejercicio de useOptimistic

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 // import { HookApp } from './HookApp'
 import './index.css'
+import { InstagromApp } from './07-use_Optimistic/instagromApp'
 // import { ScrambleWords } from './o5-useReducer/ScrambleWords'
 // import { TrafficLight } from './01-useState/TrafficLight'
 // import { TrafficLightWithEffect } from './02-useEfect/TrafficLightWithEffect'
 // import { TrafficLightWithHook } from './02-useEfect/TrafficLightWithHook'
 // import { PokemonPage } from './03-examples/PokemonPage'
 // import { TasksApp } from './o5-useReducer/TaskApp'
-import { MemoHook } from './06-memo-hook/MemoHook'
+// import { MemoHook } from './06-memo-hook/MemoHook'
 
 
 createRoot(document.getElementById('root')!).render(
@@ -20,7 +21,7 @@ createRoot(document.getElementById('root')!).render(
     {/* <PokemonPage/> */}
     {/* <TasksApp /> */}
     {/* <ScrambleWords /> */}
-    <MemoHook />
-
+    {/* <MemoHook /> */}
+    <InstagromApp />
   </StrictMode>,
 )
