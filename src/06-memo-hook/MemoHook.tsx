e71feb17794ccc6c3b25b435e26c540c1d930103
fguedez1311 <46085315+fguedez1311@users.noreply.github.com>
2025-09-12 07:37:26 -0400

    Memorización de funciones

--- src/06-memo-hook/MemoHook.tsx
+++ src/06-memo-hook/MemoHook.tsx
@@ -1,17 +1,24 @@
-import {useState} from 'react'
+import {useCallback, useState} from 'react'
 import { MyTitle } from './ui/MyTitle'
 import { MySubTitle } from './ui/MySubTitle'
+
+// const handleMyAPICall=(myValue:string)=>{
+//     console.log("Llamar a mi API- "+myValue)
+//   }
 
 export const MemoHook = () => {
   const [title, setTitle] = useState("Hola")
   const [subTitle, setSubTitle] = useState("Mundo")
+  const handleMyAPICall=useCallback(()=>{
+    console.log("Llamar a mi API- ",subTitle)
+  },[subTitle])
   return (
     <div className='bg-gradient flex flex-col gap-4
     '>
         <h1 className='text-2xl font-thin text-white'>Memo App</h1>
         <MyTitle title={title} />
 
-        <MySubTitle subtitle={subTitle} />
+        <MySubTitle subtitle={subTitle} callMyAPI={handleMyAPICall} />
 
         <button className='bg-blue-500 text-white px-4 py-2 rounded-md cursor-pointer'
                 onClick={()=>setTitle('Hello, ' + new Date().getTime())}
--- src/06-memo-hook/ui/MySubTitle.tsx
+++ src/06-memo-hook/ui/MySubTitle.tsx
@@ -1,16 +1,21 @@
 import { memo } from "react"
 
 interface Props{
-    subtitle:string
+    subtitle:string,
+    // callMyAPI:(myValue:string)=>void
+    callMyAPI:()=>void
+
 }
 
-export const MySubTitle =memo(({subtitle}:Props) => {
+export const MySubTitle =memo(({subtitle,callMyAPI}:Props) => {
   console.log("My SubTitle re-render")
   console.log("tarea Superpesada")
   return (
     <>
         <h6 className="text-2xl font-bold">{subtitle}</h6>
-        <button className="bg-indigo-500 text-white px-2 py-1 rounded-md cursor-pointer">
+        <button className="bg-indigo-500 text-white px-2 py-1 rounded-md cursor-pointer"
+                onClick={callMyAPI}
+        >
             Llamar a función
         </button>
     </>
