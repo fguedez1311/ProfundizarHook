5f40a5152bf04e4414799fb0f6b9867045f9621c
fguedez1311 <46085315+fguedez1311@users.noreply.github.com.>
2025-09-15 16:11:07 -0400

    Navegar entre pantallas

--- src/09-useContext/pages/about/AboutPage.tsx
+++ src/09-useContext/pages/about/AboutPage.tsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import { Link } from "react-router"
 
 export const AboutPage = () => {
+  
   return (
-    <div>AboutPage</div>
+    <div className="flex flex-col items-center justify-center min-h-screen">
+        <h1 className="text-4xl font-bold">Página sobre mi</h1>
+        <hr/>
+        <div className="flex flex-col gap-2">
+            <Link to="/profile" className="hover:text-blue-500 underline text-2xl">Perfil</Link>
+            <Link to="/login" className="hover:text-blue-500 underline text-2xl">Iniciar Sesión</Link>
+        </div>
+    </div>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,15 @@ import { createRoot } from 'react-dom/client'
 // import { HookApp } from './HookApp'
 import './index.css'
 // import { InstagromApp } from './07-use_Optimistic/instagromApp'
-// import { ScrambleWords } from './o5-useReducer/ScrambleWords'
 // import { TrafficLight } from './01-useState/TrafficLight'
 // import { TrafficLightWithEffect } from './02-useEfect/TrafficLightWithEffect'
+// import { ScrambleWords } from './o5-useReducer/ScrambleWords'
 // import { TrafficLightWithHook } from './02-useEfect/TrafficLightWithHook'
 // import { PokemonPage } from './03-examples/PokemonPage'
 // import { TasksApp } from './o5-useReducer/TaskApp'
 // import { MemoHook } from './06-memo-hook/MemoHook'
-import { ProffesionalAPP } from './09-useContext/ProffesionalAPP'
+import { ProffesionalAPP } from './09-useContext/ProffesionalApp'
+
 
 
 createRoot(document.getElementById('root')!).render(
