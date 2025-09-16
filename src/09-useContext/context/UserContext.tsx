2f409a09e2d6f0b870af8969133987e8295e135b
fguedez1311 <46085315+fguedez1311@users.noreply.github.com.>
2025-09-16 10:37:59 -0400

    Contexto de usuario-Información Global

--- src/09-useContext/context/UserContext.tsx
+++ src/09-useContext/context/UserContext.tsx
@@ -1,12 +1,39 @@
-import React, {type PropsWithChildren} from 'react'
+import React, {createContext, type PropsWithChildren,useState} from 'react'
+import type { User } from '../data/user-mock';
 // interface UserContextProps{
 //     children:React.ReactNode
 // }
+type AuthStatus='checking'|'authenticated'|'not-authenticated'
+interface UserContextProps{
+  authStatus:AuthStatus;
+  user:User | null;
+
+
+  // Methods
+  login:(userId:number)=>boolean;
+  logout:()=>void;
+}
+export const UserContext=createContext({} as UserContextProps)
 export const UserContextProvider = ({children}:PropsWithChildren) => {
-   
+    const [authState, setAuthState] = useState<AuthStatus>('checking')
+    const [user, setUser] = useState<User | null>(null)
+    const handleLogin=(userId:number)=>{
+      console.log(userId)
+      return true
+    }
+    const handleLogout=()=>{
+      console.log('logout')
+    }
     return (
-    <>
+    <UserContext value={{ 
+        authStatus:authState,
+        user:user,
+        login:handleLogin,
+        logout:handleLogout
+
+     }}
+    >
         {children}
-    </>
+    </UserContext>
   )
 }
